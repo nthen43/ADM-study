eca13d975167a75e124a27a904ed364436f5cd1a
nelson <nelsonduthen42@gmail.com>
2024-12-03 17:52:13 +1030

    finished Linked List implementation

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -5,92 +5,211 @@ using namespace std;
 class Node {
   public:
     int value;
-    Node* next = nullptr;
+    Node* next;
 
     Node(int value){
       this->value = value;
+      this->next = nullptr;
     }
 
     Node(){
       this->value = 0;
       this->next = nullptr;
     }
-
-    bool empty(Node* head){
-      if(head->next == nullptr){
-        return true;
-      }
-      return false;
-    }
-
-    void traverse(Node* node){
-      Node* current = node;
-
-      while(current->next != nullptr){
-        cout << current->value << endl;
-        current = current->next;
-      }
-
-      cout << current->value << endl;
-      cout << "this is the end of the linked list." << endl;
-    }
-
-    Node* searchForNode(Node* head, int target){
-      Node* current = head->next;
-      while(current->next != nullptr){
-        if(current->value == target){
-          return current;
-        }
-        current = current->next;
-      }
-      return nullptr; // if node is not found, return a nullptr.
-    }
-
-    Node* insertNodeAtStart(Node* head, int value){
-      Node* newNode = new Node(value);
-      newNode->next = head;
-      head = newNode;
-      return head;
-    }
-
-    void insertNodeAtEnd(Node* head, int value){
-      Node* newNode = new Node(value);
-      Node* current = head;
-      while(current->next != nullptr){
-        current = current->next;
-      }
-
-      current->next = newNode;
-      newNode->next = nullptr;
-    }
-
-    void insertNodeAnywhere(Node* head, int value, int pos){
-      
-    }
 };
 
+class LinkedList {
+  private:
+  Node* head;
+
+  public:
+
+  LinkedList(){
+    this->head = nullptr;
+  }
+
+  ~LinkedList(){
+    while(this->head!= nullptr) {
+      Node* temp = this->head;
+      this->head= this->head->next;
+      delete temp;
+    }
+  }
+
+  bool empty(){
+    return this->head == nullptr;
+  }
+
+  void traverse(){
+    if(empty()) {
+      cout << "list is empty." << endl;
+      return;
+    }
+
+    Node* current = this->head;
+
+    while(current != nullptr){
+      cout << current->value << " ";
+      current = current->next;
+    }
+
+    cout << endl;
+    cout << endl;
+  }
+
+  Node* searchForNode(int target){
+    Node* current = this->head;
+    while(current->next != nullptr){
+      if(current->value == target){
+        return current;
+      }
+      current = current->next;
+    }
+    return nullptr; // if node is not found, return a nullptr.
+  }
+
+  void insertNodeAtStart(int value){
+    Node* newNode = new Node(value);
+    newNode->next = this->head;
+    this->head = newNode;
+  }
+
+  void insertNodeAtEnd(int value){
+    Node* newNode = new Node(value);
+    if(empty()){
+      this->head = newNode;
+      return;
+    }
+
+    Node* current = this->head;
+    while(current->next != nullptr){
+      current = current->next;
+    }
+
+    current->next = newNode;
+  }
+
+  void insertNodeAnywhere(int value, int pos){
+    if(pos < 0){
+      cout << "invalid position" <<  endl;
+      return;
+    }
+
+    if(pos == 0){
+      insertNodeAtStart(value);
+      return;
+    }
+
+    Node* newNode = new Node(value);
+    int counter = 0;
+    Node* predecessor = this->head;
+    while(counter < pos - 1 && predecessor != nullptr){ // traverse until the node before our position
+      predecessor = predecessor->next;
+      counter++;
+    }
+
+    if(predecessor == nullptr){
+      cout << "invalid position" << endl;
+      delete newNode;
+      return;
+    }
+
+    newNode->next = predecessor->next; // new node will point to what our previous node USED to point to
+    predecessor->next = newNode; // our predecessor will point to the new node we just made.
+  }
+
+  void deleteAtStart(){
+    if(empty()){
+      cout << "List is Empty." << endl;
+      return;
+    }
+
+    Node* temp = this->head;
+    this->head = this->head->next;
+    delete temp;
+  }
+
+  void deleteAtEnd(){
+    if(empty()){ // if this->head is nullptr, list does not exist
+      cout << "List is Empty." << endl;
+      return;
+    }
+
+    if(this->head->next == nullptr){ // if the next node is null, then this->head is the only node in list
+      delete this->head;
+      this->head = nullptr;
+      return;
+    }
+
+    Node* secondLast = this->head;
+    while(secondLast->next->next != nullptr){
+      secondLast = secondLast->next; // traverse to end of list - 1
+    }
+
+    delete (secondLast->next);
+    secondLast->next = nullptr;
+  }
+
+  void deleteAnywhere(int position){
+    if(empty() || position < 0){
+      cout << "list empty or position invalid" << endl;
+      return;
+    }
+
+    if(position == 0) {
+      deleteAtStart();
+      return;
+    }
+
+    int counter = 0;
+    Node* predecessor = this->head;
+    
+    while(counter < position - 1 && predecessor->next != nullptr){
+      predecessor = predecessor->next;
+      counter++;
+    }
+
+    if(predecessor->next == nullptr){
+      cout << "invalid position" << endl;
+      return;
+    }
+
+    Node* toDelete = predecessor->next; // node to be deleted is the one after our predecessor
+    predecessor->next = predecessor->next->next; // move our pointer to the node after the one to be deleted
+    delete toDelete;
+  }
+};
 
 int main(){
-  Node head;
+  LinkedList myList;
 
-  Node node1(1);
-  Node node2(2);
-  Node node3(3);
-  Node node4(4);
+  myList.insertNodeAtEnd(1);
+  myList.insertNodeAtEnd(2);
+  myList.insertNodeAtEnd(3);
+  myList.insertNodeAtEnd(4);
 
-  head.next = &node1;
-  node1.next = &node2;
-  node2.next = &node3;
-  node3.next = &node4;
-  node4.next = nullptr;
+  cout << "Initial List: " << endl;
+  myList.traverse();
 
-  head.traverse(head.next);
-  cout << head.searchForNode(&head, 1)->value << endl; // print the value of the node we are searching for (in this case 1)
+  cout << "Insert 0 at the start of LL" << endl;
+  myList.insertNodeAtStart(0);
+  myList.traverse();
 
-  cout<< endl;
-  Node* newHead = head.insertNodeAtStart(&head, -1); // insert at the head, thus becoming the new head
-  head.insertNodeAtEnd(&head, 5); // insert node 5
-  head.traverse(newHead); // traverse through list printing everything to see if it worked.
+  cout << "Inserting 34 in between 3 and 4 (position 4)" << endl;
+  myList.insertNodeAnywhere(34, 4);
+  myList.traverse();
+
+  cout << "Deleting at Start (0)" << endl;
+  myList.deleteAtStart();
+  myList.traverse();
+
+  cout << "Deleting at the End (4)" << endl;
+  myList.deleteAtEnd();
+  myList.traverse();
+
+  cout << "Delete at position 1 (2)" << endl;
+  myList.deleteAnywhere(1);
+  myList.traverse(); 
 
   return 0;
 }
